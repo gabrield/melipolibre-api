22bc50e4ea58605f7804e27988b64094cf6820fb
Gabriel Duarte <gabs.ansf@gmail.com>
2023-03-13 17:01:00 -0300

    adiçao do parametrize para multiplos valores invalidos na criação do modelo beekeeper

--- tests/unit/test_beekeeper_model.py
+++ tests/unit/test_beekeeper_model.py
@@ -6,16 +6,22 @@ from app.models.bee_model import BeeModel
 from factories import BeeKeeperFactory
 
 def test_create_one_beekeeper_model(db):
-    beekeeper = BeeKeeperFactory(name='Gabs', email='gabs@abelha.cc', password='123')
+    beekeeper = BeeKeeperFactory(name='Gabs', email='gabs@melipolibre.cc', password='123')
     assert beekeeper.name == 'Gabs'
-    assert beekeeper.email == 'gabs@abelha.cc'
+    assert beekeeper.email == 'gabs@melipolibre.cc'
     assert beekeeper.password == '123'
     assert beekeeper.active == False
 
-
-def test_create_one_beekeeper_model_without_password(db):
+@pytest.mark.parametrize("name, email, password", [
+    ('Gabs', 'gabs@melipolibre.cc', None),
+    ("Test", None ,"name"),
+    (None, None, 'pwdtest'),
+    (None, None, None),
+    ("Daniel", None, None)
+])
+def test_create_beekeeper_with_missing_parameters(db, name, email, password):
     '''Must raise an exception by miscreating BeeKeeperModel'''
     with pytest.raises(sqlalchemy.exc.IntegrityError) as e:
-        beekeeper = BeeKeeperModel(name='Gabs', email='gabs@abelha.cc')
+        beekeeper = BeeKeeperModel(name=name, email=email, password=password)
         db.session.add(beekeeper)
         db.session.commit()
