9a11107dbe3861b4bbea1564c17438dc848b2d02
Gabriel Duarte <gabs.ansf@gmail.com>
2023-09-22 12:56:40 -0300

    indentation fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
-import os, json, pytest
+import os
+import json
+import pytest
 from app import create_app
 from app.database import db as _db
 from app.models.bee_model import BeeModel
@@ -11,12 +13,10 @@ def app():
     yield app
 
 
-
-
 @pytest.fixture(scope='module')
 def client(app):
     with app.test_client() as client:
-         with app.app_context():
+        with app.app_context():
             _db.drop_all()
             _db.create_all()
             yield client
@@ -28,22 +28,22 @@ def db(app):
     _db.init_app(app)
     _db.create_all()
     print(os.path.dirname(os.path.abspath(__file__)))
-    with open(os.path.abspath('.') + \
-            '/app/data/asf_list.json', encoding='utf-8') as bee_file:
-            bees =  json.load(bee_file)
-            _db.session.bulk_save_objects([BeeModel(**bee) for bee in bees])
-            _db.session.commit()
+    with open(os.path.abspath('.') +
+              '/app/data/asf_list.json', encoding='utf-8') as bee_file:
+        bees = json.load(bee_file)
+        _db.session.bulk_save_objects([BeeModel(**bee) for bee in bees])
+        _db.session.commit()
     yield _db
     _db.session.close()
     _db.drop_all()
 
 
-
 @pytest.fixture
 def beekeeper(beekeeper_stub):
     '''Creates BeeKeeper on database and returns its plain __dict_ representation'''
-    BeeKeeperFactory(**beekeeper_stub) #create registered user
+    BeeKeeperFactory(**beekeeper_stub)  # create registered user
     return beekeeper_stub
+
 
 @pytest.fixture
 def beekeeper_stub():
