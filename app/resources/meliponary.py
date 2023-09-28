b322c6e84e3d4dc4f4236ddc0b08ff55b480613a
Gabriel Duarte <gabs.ansf@gmail.com>
2023-09-27 22:44:23 -0300

    lint

--- app/resources/meliponary.py
+++ app/resources/meliponary.py
@@ -7,68 +7,66 @@ params = reqparse.RequestParser()
 params.add_argument('name', type=str, required=True, trim=True)
 params.add_argument('address', type=str, required=True, trim=True)
 
-class Meliponaries(Resource):
-        @jwt_required()
-        def get(self): #TODO: filter by name and/or address
-            return {
-                    'meliponaries':
-                    [meliponary.json() for meliponary in current_user.meliponaries]
-            }, 200
 
-    
-        @jwt_required()
-        def post(self):
-            _params = params.parse_args()
-            meliponary = MeliponaryModel(**_params, beekeeper=current_user)
-            db.session.add(meliponary)
-            db.session.commit()
-            return {'message' : f'{meliponary.name} created!'}, 201
+class Meliponaries(Resource):
+    @jwt_required()
+    def get(self):  # TODO: filter by name and/or address
+        return {
+            'meliponaries':
+                [meliponary.json() for meliponary in current_user.meliponaries]
+        }, 200
+
+    @jwt_required()
+    def post(self):
+        _params = params.parse_args()
+        meliponary = MeliponaryModel(**_params, beekeeper=current_user)
+        db.session.add(meliponary)
+        db.session.commit()
+        return {'message': f'{meliponary.name} created!'}, 201
 
 
 class Meliponary(Resource):
-        @jwt_required()
-        def get(self, meliponary_id):
-            meliponary = MeliponaryModel.query.get(meliponary_id)
+    @jwt_required()
+    def get(self, meliponary_id):
+        meliponary = MeliponaryModel.query.get(meliponary_id)
 
-            if meliponary:
-                if meliponary.beekeeper_id != current_user.id:
-                    return {'message' : f'Meliponary {meliponary_id} doesn\'t belong to user'}, 401
-                return meliponary.json(), 200
-            
-            return {'message' : f'Meliponary {meliponary_id} doesn\'t exist'}, 404
+        if meliponary:
+            if meliponary.beekeeper_id != current_user.id:
+                return {'message': f'Meliponary {meliponary_id} doesn\'t belong to user'}, 401
+            return meliponary.json(), 200
 
-        @jwt_required()
-        def put(self, meliponary_id):
-            meliponary = MeliponaryModel.query.filter_by(id=meliponary_id).one_or_none()
+        return {'message': f'Meliponary {meliponary_id} doesn\'t exist'}, 404
 
-            if meliponary:
-                if meliponary.beekeeper_id != current_user.id:
-                    return {'message' : f'Meliponary {meliponary_id} doesn\'t belong to user'}, 403
+    @jwt_required()
+    def put(self, meliponary_id):
+        meliponary = MeliponaryModel.query.filter_by(
+            id=meliponary_id).one_or_none()
 
-                _params = params.parse_args()
+        if meliponary:
+            if meliponary.beekeeper_id != current_user.id:
+                return {'message': f'Meliponary {meliponary_id} doesn\'t belong to user'}, 403
 
-                meliponary.name = _params['name']
-                meliponary.address = _params['address']
-                db.session.commit()
-                return {'message' : f'Meliponary {meliponary.name} updated'}, 200
-        
-            return {'message' : f'Meliponary {meliponary_id} doesn\'t exist'}, 404
-    
-        @jwt_required()
-        def delete(self, meliponary_id):
-            #fix: filter using current_user.meliponaries.filter_by ???
-            meliponary = current_user.meliponaries.filter_by(id=meliponary_id).one_or_none()
+            _params = params.parse_args()
 
-            if meliponary:
-                if meliponary.beekeeper_id != current_user.id:
-                    return {'message' : f'Meliponary {meliponary_id} doesn\'t belong to user'}, 403
-                
-                MeliponaryModel.query.filter_by(id=meliponary_id).delete()
-                db.session.commit()
-                return {'message' : f'Meliponary {meliponary.name} deleted'}, 200
-            
-            return {'message' : f'Meliponary {meliponary_id} doesn\'t exist'}, 404
+            meliponary.name = _params['name']
+            meliponary.address = _params['address']
+            db.session.commit()
+            return {'message': f'Meliponary {meliponary.name} updated'}, 200
 
+        return {'message': f'Meliponary {meliponary_id} doesn\'t exist'}, 404
 
-                
-            
+    @jwt_required()
+    def delete(self, meliponary_id):
+        # fix: filter using current_user.meliponaries.filter_by ???
+        meliponary = current_user.meliponaries.filter_by(
+            id=meliponary_id).one_or_none()
+
+        if meliponary:
+            if meliponary.beekeeper_id != current_user.id:
+                return {'message': f'Meliponary {meliponary_id} doesn\'t belong to user'}, 403
+
+            MeliponaryModel.query.filter_by(id=meliponary_id).delete()
+            db.session.commit()
+            return {'message': f'Meliponary {meliponary.name} deleted'}, 200
+
+        return {'message': f'Meliponary {meliponary_id} doesn\'t exist'}, 404
