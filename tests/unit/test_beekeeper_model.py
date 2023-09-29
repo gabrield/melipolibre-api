1b45389e1dd31b739ae9bf01b60e23114b1a0ebe
Gabriel Duarte <gabs.ansf@gmail.com>
2023-09-29 17:49:02 -0300

    More tests added to beekeeper model

--- tests/unit/test_beekeeper_model.py
+++ tests/unit/test_beekeeper_model.py
@@ -1,20 +1,24 @@
-import pytest, sqlalchemy
+import pytest
+import sqlalchemy
 from app.models.beekeeper_model import BeeKeeperModel
 from app.models.meliponary_model import MeliponaryModel
 from app.models.beehive_model import BeeHiveModel, BeeHiveType
 from app.models.bee_model import BeeModel
 from factories import BeeKeeperFactory
 
+
 def test_create_one_beekeeper_model(db):
-    beekeeper = BeeKeeperFactory(name='Gabs', email='gabs@melipolibre.cc', password='123')
+    beekeeper = BeeKeeperFactory(
+        name='Gabs', email='gabs@melipolibre.cc', password='123')
     assert beekeeper.name == 'Gabs'
     assert beekeeper.email == 'gabs@melipolibre.cc'
     assert beekeeper.password == '123'
     assert beekeeper.active == False
 
+
 @pytest.mark.parametrize("name, email, password", [
     ('Gabs', 'gabs@melipolibre.cc', None),
-    ("Test", None ,"name"),
+    ("Test", None, "name"),
     (None, None, 'pwdtest'),
     (None, None, None),
     ("Daniel", None, None)
@@ -25,6 +29,25 @@ def test_create_beekeeper_with_missing_parameters(db, name, email, password):
         beekeeper = BeeKeeperModel(name=name, email=email, password=password)
         db.session.add(beekeeper)
         db.session.commit()
+
+
+def test_create_one_beekeeper_model_and_save_to_db(db):
+    try:
+        beekeeper = BeeKeeperFactory(
+            name='Gabs', email='gabs@melipolibre.cc', password='123')
+        db.session.add(beekeeper)
+        db.session.commit()
+    except Exception:
+        raise Exception("test_create_one_beekeeper_model_and_save_to_db")
+
+
+def test_create_one_beekeeper_model_and_retrieve_from_db(db):
+    beekeeper = BeeKeeperFactory(
+        name='Gabs', email='gabs@melipolibre.cc', password='123')
+    db.session.add(beekeeper)
+    db.session.commit()
+    assert db.session.query(BeeKeeperModel).filter(
+        BeeKeeperModel.email == beekeeper.email).one() == beekeeper
 
 
 '''
